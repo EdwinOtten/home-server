a52cd1cd2c4b6c779b6e89c42dced1fdfad7b11f
edwin <EdwinOtten@users.noreply.github.com>
2025-12-17 20:18:44 +0100

    update media-server compose file

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@
 
 mkdir /opt/home-server-data
 mkdir /opt/configarr-cache
+mkdir /opt/sonarr-config
 
 chown -R rogs:rogs /opt/home-server-data
 chown -R rogs:rogs /opt/configarr-cache
+chown -R rogs:rogs /opt/sonarr-config
 
 #cp ./media-server/config /opt/media-server-config
 
