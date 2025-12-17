97a0f8182417c0fe89870393d16eb7f7293eb79b
edwin <EdwinOtten@users.noreply.github.com>
2025-12-17 20:08:16 +0100

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir /opt/home-server-data
+mkdir /opt/configarr-cache
+
+chown -R rogs:rogs /opt/home-server-data
+chown -R rogs:rogs /opt/configarr-cache
+
+#cp ./media-server/config /opt/media-server-config
+
