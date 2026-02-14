074037d9a34f201fb06e42a503e4f80915aa9a75
Edwin Otten <EdwinOtten@users.noreply.github.com>
2026-02-14 23:53:14 +0100

    fix: setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,16 @@ if [[ ! -z "$2" ]]; then
     GROUP_NAME="$2"
 fi
 
-# Example chown commands
-chown "$USER_NAME:$GROUP_NAME" /path/to/file1
-chown "$USER_NAME:$GROUP_NAME" /path/to/file2
+# create config directories
+mkdir /opt/home-server-data
+mkdir /opt/configarr-cache
+mkdir /opt/sonarr-config
+
+# set owner on config directories
+chown -R "$USER_NAME:$GROUP_NAME" /opt/home-server-data
+chown -R "$USER_NAME:$GROUP_NAME" /opt/configarr-cache
+chown -R "$USER_NAME:$GROUP_NAME" /opt/sonarr-cache
+
 
 # Additional commands can follow...
+#cp ./media-server/config /opt/media-server-config
